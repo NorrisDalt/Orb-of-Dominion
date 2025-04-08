f5ad078968461d4bf24e3d8a2bc9706580923fed
shototype <mattnewmanfrfr@gmail.com>
2025-04-08 14:59:07 -0400

    Added Basic Main Menu with menu nav script

---
+++ Project_3/Assets/Scripts/Menu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuParent;
+    public GameObject mainCreditsParent;
+    public GameObject mainControlsParent;
+    //public GameObject creditsMain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadPlayGame()
+    {
+        SceneManager.LoadScene("forestlevel");
+    }
+
+    public void LoadQuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenCreditsMain()
+    {
+        mainMenuParent.SetActive(false);
+        mainCreditsParent.SetActive(true);
+    }
+
+    public void CloseCreditsMain()
+    {
+        mainMenuParent.SetActive(true);
+        mainCreditsParent.SetActive(false);
+    }
+
+    public void OpenControlsMain()
+    {
+        mainMenuParent.SetActive(false);
+        mainControlsParent.SetActive(true);
+    }
+
+    public void CloseControlsMain()
+    {
+        mainMenuParent.SetActive(true);
+        mainControlsParent.SetActive(false);
+    }
+}
